d21f6bb6c63c226aac14a0a1293c3e0fd9a853f0
Arthur Sena <149202391+devthur7@users.noreply.github.com>
2025-08-22 14:14:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let nickName = prompt("Seu nome!");
-let numeroXp = Number(prompt("Quantidade de XP"));
-let nivel = "";
+function nivelPlay() {
+    let saldoVitoria = Number(prompt("Quantas vitorias ?"));
+    let nivel = "";
 
-if (numeroXp < 1000) {
-    nivel = "Ferro";
-} else if (numeroXp >= 1001 && numeroXp <= 2000) {
-    nivel = "Bronze";
-} else if (numeroXp >= 2001 && numeroXp <= 5000) {
-    nivel = "Prata";
-} else if (numeroXp >= 5001 && numeroXp <= 7000) {
-    nivel = "Ouro";
-} else if (numeroXp >= 7001 && numeroXp <= 8000) {
-    nivel = "Platina";
-} else if (numeroXp >= 8001 && numeroXp <= 9000) {
-    nivel = "Ascendente";
-} else if (numeroXp >= 9001 && numeroXp <= 10000) {
-    nivel = "Imortal";
-} else (numeroXp >= 10001) {
-    nivel = "Radiante";
+    if (saldoVitoria < 10) {
+        nivel = "Ferro";
+    } else if (saldoVitoria >= 11 && saldoVitoria <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitoria >= 21 && saldoVitoria <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitoria >= 51 && saldoVitoria <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitoria >= 81 && saldoVitoria <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitoria >= 91 && saldoVitoria <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O herói tem um saldo de ${saldoVitoria} vitórias e está no nível ${nivel}`);
 }
 
-console.log(O Herói de nome ${nickName} está no nível de ${nivel});
+nivelPlay();
